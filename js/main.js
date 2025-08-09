5eeb75c7fcdc7cebca6cb0bfed04021fcd283a1a
parksangseok <park0157@naver.com>
2025-08-09 16:04:32 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -8,69 +8,67 @@ $(function () {
     });
   });
 
-$("#reviewWrap .slick").slick({
-  variableWidth: true,
-  autoplay: true,
-  arrows: true,
-  dots: false,
-  accessibility: false,
-  draggable: true,
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  zIndex: 1000,
-  pauseOnHover: false,
-  autoplaySpeed: 6000,
-  centerMode: true,
-  speed: 1500,
-  prevArrow: $('.mainBox .leftBox .prev'),
-  nextArrow: $('.mainBox .leftBox .next'),
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        centerMode: false
+  //후기 슬라이드
+  $("#reviewWrap .slick").slick({
+    variableWidth: true,
+    autoplay: true,
+    arrows: true,
+    dots: false,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 6000,
+    centerMode: true,
+    speed: 1500,
+    prevArrow: $('.mainBox .leftBox .prev'),
+    nextArrow: $('.mainBox .leftBox .next'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          centerMode: false
+        }
       }
-    }
-  ]
-});
+    ]
+  });
 
-  // $('.mainBox .leftBox .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-  //   var i = (currentSlide ? currentSlide : 0) + 1;
-  //   $('.mainBox .leftBox .count').html('<em>' + i + '</em> / ' + slick.slideCount);
+  //탭 슬라이드
+  $('#infoWrap03 .tabBox .tabBtn').on('click', function () {
 
-  //   var $text = $('.mainBox .leftBox .slideText');
-  //   $text.removeClass('active').eq(currentSlide || 0).addClass('active');
-  // });
+    let index = $(this).index();
 
-  // $(".mainBox .leftBox .slick").slick({
-  //   autoplay: true,
-  //   arrows: true,
-  //   dots: false,
-  //   accessibility: false,
-  //   draggable: true,
-  //   infinite: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   zIndex: 1000,
-  //   pauseOnHover: false,
-  //   autoplaySpeed: 6000,
-  //   speed: 1500,
-  //   prevArrow: $('.mainBox .leftBox .prev'),
-  //   nextArrow: $('.mainBox .leftBox .next'),
-  // });
+    $('#infoWrap03 .slick').slick('slickGoTo', index);
+    $('#infoWrap03 .tabBox .tabBtn').removeClass('active');
+    $(this).addClass('active');
 
-  // $('.mainBox .leftBox .control button').on('click', function (e) {
-  //   e.preventDefault();
-  //   if ($(this).hasClass('pause')) {
-  //     $(this).hide();
-  //     $('.mainBox .leftBox .control .play').show();
-  //     $('.mainBox .leftBox .slick').slick('slickPause');
-  //   } else if ($(this).hasClass('play')) {
-  //     $(this).hide();
-  //     $('.mainBox .leftBox .control .pause').show();
-  //     $('.mainBox .leftBox .slick').slick('slickPlay');
-  //   }
-  // });
+  });
+
+  $('#infoWrap03 .slick').on('afterChange', function (event, slick, currentSlide) {
+    $('#infoWrap03 .tabBox .tabBtn').removeClass('active');
+    $('#infoWrap03 .tabBox .tabBtn').eq(currentSlide).addClass('active');
+  });
+
+  $("#infoWrap03 .slick").slick({
+    autoplay: true,
+    arrows: true,
+    dots: false,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 5000,
+    speed: 1500,
+    prevArrow: $('#infoWrap03 .prev'),
+    nextArrow: $('#infoWrap03 .next'),
+  });
+
+
 
 });
